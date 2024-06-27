f28b652e87d33370791bb8b0bb44dd027d06bac3
Zixiong Lin Yang <zlinyan@gesplan.es>
2024-06-27 10:24:24 +0100

    Añadido los speedInsights de Vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
 import { NavBar } from "./components/Navbar";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const MPLUS1 = M_PLUS_Rounded_1c({
   weight: ["700","900"],
@@ -26,6 +27,7 @@ export default function RootLayout({
         <NavBar />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
