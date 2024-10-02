74066007b9b4f78dde029806bc28663fb232e37b
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-02 12:51:35 +0100

    Add new footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,22 @@ import { Description } from "./sections/02-description";
 import { Targets } from "./sections/03-targets";
 import { Tools } from "./sections/04-tools";
 import { Actions } from "./sections/05-actions";
+import { Footer } from "./sections/06-footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <Hero></Hero>
-      <Description></Description>
-      <Targets></Targets>
+    <>
+      <main className="flex min-h-screen flex-col items-center">
+        <Hero></Hero>
+        <Description></Description>
+        <Targets></Targets>
 
-      <section className="h-6"></section>
-      <Tools></Tools>
-      <Actions></Actions>
-      <section className="h-28"></section>
-    </main>
+        <section className="h-6"></section>
+        <Tools></Tools>
+        <Actions></Actions>
+        <section className="h-28"></section>
+      </main>
+      <Footer></Footer>
+    </>
   );
 }
