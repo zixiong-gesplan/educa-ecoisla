eadd5c443bab48780b50ea98c485dacab3608055
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-02 13:23:20 +0100

    Fixed Phone Number

--- app/sections/06-footer.tsx
+++ app/sections/06-footer.tsx
@@ -3,7 +3,7 @@ import { FooterBase } from "../components/Footer"
 export const Footer = () => {
     return <section className="mt-10 mx-0 text-center">
 
-        <FooterBase telephone="928-412343" text={`${new Date().getFullYear()} Gesplan. Todos los derechos reservados.`} />
+        <FooterBase telephone="628 53 20 29" text={`${new Date().getFullYear()} Gesplan. Todos los derechos reservados.`} />
 
     </section>
 }
