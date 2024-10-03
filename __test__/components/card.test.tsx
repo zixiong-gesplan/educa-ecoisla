35fc6930186ac5620ae59471ee325e2d5dd6cde3
Zixiong Lin Yang <zlinyan@gesplan.es>
2024-10-03 12:02:41 +0100

    Added Card testing

---
+++ __test__/components/card.test.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@/app/components/Card";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+describe("Card component", () => {
+    it("render", () => {
+        render(<Card
+            title="carta"
+            description="lorem ipsum"
+        />)
+        expect(screen.getByText("carta")).not.toBeNull()
+    })
+})
