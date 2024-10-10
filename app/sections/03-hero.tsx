0649c7a1ba8410bfefcd338d15ff5cc8a0eadea4
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-10 13:54:05 +0100

    Add custom button component

--- app/sections/03-hero.tsx
+++ app/sections/03-hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { CustomButton } from "../components/CustomButton";
 
 export const Hero = () => {
 	return (
@@ -10,18 +11,29 @@ export const Hero = () => {
 						para la Sostenibilidad, centrado en la concienciación social sobre la conservación del medio ambiente.
 					</p>
 					<div className="mt-6 w-[200px] lg:w-2/3 flex justify-start gap-2" >
-						<button className="flex items-center justify-between text-white bg-custom-green-800 hover:bg-custom-green-900 rounded-lg mr-1 px-4 py-2 w-60 lg:w-[300px]">
-							<div className="w-2/3 text-center flex justify-center items-center text-sm md:text-base">Galería de Actividades</div>
-							<svg className="w-12 h-12 text-white dark:text-white content-center ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z" />
-							</svg>
-						</button>
-						<button className="flex items-center justify-between text-custom-dark-green bg-custom-light-bg bg-opacity-20 hover:bg-opacity-40 rounded-lg px-4 py-2 w-60 lg:w-[300px]">
-							<div className="w-2/3 text-center flex justify-center items-center text-sm md:text-base">Próxima actividad</div>
-							<svg className="w-12 h-12 text-custom-dark-green dark:text-white content-center ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778" />
-							</svg>
-						</button>
+
+						<CustomButton
+							content="Galería de Actividades"
+							textcolor="text-white"
+							bg="bg-custom-green-800"
+							opa=""
+							opahover="hover:bg-custom-green-900"
+							width="w-2/3"
+							d="m3 16 5-7 6 6.5m6.5 2.5L16 13l-4.286 6M14 10h.01M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"
+						>
+						</CustomButton>
+
+						<CustomButton
+							content="Próxima actividad"
+							textcolor="text-custom-dark-green"
+							bg="bg-custom-light-bg"
+							opa="bg-opacity-20"
+							opahover="hover:bg-opacity-40"
+							width="w-2/3"
+							d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"
+						>
+						</CustomButton>
+
 					</div>
 				</div>
 				<div className="hidden xl:mt-0 xl:col-span-5 xl:flex ml-4">
