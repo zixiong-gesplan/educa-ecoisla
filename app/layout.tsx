c443151d85891860e9334000cfe5f0f09502cd81
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-10 12:39:52 +0100

    Split hearder and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,37 @@
 import type { Metadata } from "next";
 import { Inter, M_PLUS_Rounded_1c } from "next/font/google";
 import "./globals.css";
-import { NavBar } from "./components/Navbar";
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
+import { Header } from "./sections/01-header";
+import { NavBar } from "./sections/02-navbar";
+
 const MPLUS1 = M_PLUS_Rounded_1c({
-  weight: ["700","900"],
-  subsets: ["latin"],
-  style: "normal",
- });
+	weight: ["700", "900"],
+	subsets: ["latin"],
+	style: "normal",
+});
 
 export const metadata: Metadata = {
-  title: "Educa Ecoisla",
-  description: "Web para la divulgación del proyecto de Educa Ecoisla",
+	title: "Educa Ecoisla",
+	description: "Web para la divulgación del proyecto de Educa Ecoisla",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="es">
-      <body className={MPLUS1.className}>
-        <NavBar />
-        {children}
-        <Analytics />
-        <SpeedInsights />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="es">
+			<body className={MPLUS1.className}>
+				<Header></Header>
+				<NavBar></NavBar>
+				{children}
+				<Analytics />
+				<SpeedInsights />
+			</body>
+		</html>
+	);
 }
