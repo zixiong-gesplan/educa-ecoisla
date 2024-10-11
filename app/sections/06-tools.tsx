e83a05422a8811b77deb6dd81dfc7f14ff327dca
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-11 11:16:06 +0100

    Modify show and hide images in tool section

--- app/sections/06-tools.tsx
+++ app/sections/06-tools.tsx
@@ -13,44 +13,49 @@ export const Tools = () => {
 				<p className="border-b-2 border-custom-light-bg py-4 text-lg sm:text-xl">
 					Promover actividades educativas destinadas a la ciudadanína de Gran Canaria mediante el ámbito formal y no formal
 					(centros escolares, colectivos, administraciones locales, etc.),
-					a través de acciones como salidas de campo, campañas de sensibilización , jornadas de voluntariado, etc.				
+					a través de acciones como salidas de campo, campañas de sensibilización , jornadas de voluntariado, etc.
 				</p>
 				<ul className="mt-4 flex flex-col gap-4">
-					<li className="flex gap-2"> <SVGTicket/> Salidas al campo</li>
-					<li className="flex gap-2"> <SVGTicket/> Campañas de sensibilización</li>
-					<li className="flex gap-2"> <SVGTicket/> Voluntariado</li>
+					<li className="flex gap-2"> <SVGTicket /> Salidas al campo</li>
+					<li className="flex gap-2"> <SVGTicket /> Campañas de sensibilización</li>
+					<li className="flex gap-2"> <SVGTicket /> Voluntariado</li>
 				</ul>
 			</div>
-			<Image
+			<div className="hidden xl:mt-0 xl:block xl:w-1/3">
+				<Image
 					src="/informacion.png"
 					alt="Imágen de portada"
 					unoptimized={true}
 					width={0}
 					height={0}
-					className="w-1/3 rounded-2xl"
-			/>
+					className="w-full rounded-2xl"
+				/>
+			</div>
 		</Article>
 		<Article direction="flex-row-reverse">
 			<div className="w-full xl:w-2/3">
-			<h3 className="text-2xl sm:text-3xl text-custom-light-bg">Divulgación ambiental</h3>
+				<h3 className="text-2xl sm:text-3xl text-custom-light-bg">Divulgación ambiental</h3>
 				<p className="border-b-2 border-custom-light-bg py-4 text-lg sm:text-xl">
-				Mediante el que se elaboran materiales divulgativos, promoviendo la difusión
-				de contenidos para el fomento de la concienciación social en determinados ámbitos.
+					Mediante el que se elaboran materiales divulgativos, promoviendo la difusión
+					de contenidos para el fomento de la concienciación social en determinados ámbitos.
 				</p>
 				<ul className="mt-4 flex flex-col gap-4">
-					<li className="flex gap-2"> <SVGTicket/> Materiales divulgativos</li>
-					<li className="flex gap-2"> <SVGTicket/> Difusión de contenido</li>
-					<li className="flex gap-2"> <SVGTicket/> Concienciación social</li>
+					<li className="flex gap-2"> <SVGTicket /> Materiales divulgativos</li>
+					<li className="flex gap-2"> <SVGTicket /> Difusión de contenido</li>
+					<li className="flex gap-2"> <SVGTicket /> Concienciación social</li>
 				</ul>
 			</div>
-			<Image
+			<div className="hidden xl:mt-0 xl:block xl:w-1/3">
+				<Image
 					src="/informacion.png"
 					alt="Imágen de portada"
 					unoptimized={true}
 					width={0}
 					height={0}
-					className="w-1/3 rounded-2xl"
-			/>
-		</Article>		
-	</section>
+					className="w-full rounded-2xl"
+				/>
+			</div>
+
+		</Article>
+	</section >
 }
