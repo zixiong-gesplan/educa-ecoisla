c8c965f1663c78a8ad6373cbb53be166b0e798a7
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-14 09:28:00 +0100

    Add responsive to Ejes de actuación

--- app/sections/05-targets.tsx
+++ app/sections/05-targets.tsx
@@ -4,9 +4,9 @@ import { Target } from "../components/Target"
 export const Targets = () => {
     return (<section>
 
-        <h3 className="text-center text-3xl sm:text-4xl text-custom-light-bg font-bold mt-8">Ejes de Actuación</h3>
+        <h3 className="text-center text-2xl sm:text-3xl text-custom-light-bg bg-custom-light-bg bg-opacity-10 rounded-lg mx-3 md:bg-white font-bold mt-8 p-3 ">EJES DE ACTUACIÓN</h3>
 
-        <div className="mt-2 py-4 lg:px-14 px-4 md:px-12 gap-8 grid md:grid-cols-1 xl:grid-cols-3 auto-cols-[minmax(300px,_1fr)]">
+        <div className="mt-2 py-4 lg:px-14 px-4 md:px-12 gap-3 md:gap-8 grid md:grid-cols-1 xl:grid-cols-3 auto-cols-[minmax(300px,_1fr)]">
 
             <Card
                 title="Patrimonio Natural de Gran Canaria"
@@ -28,6 +28,10 @@ export const Targets = () => {
                 actividades que incentiven el consumo responsable de recursos, la eficiencia energética, la
                 separación de los residuos, etc."
             />
+            <div className="py-12 px-4 bg-[#83AC50] text-[#ffffff] rounded-lg	sm:min-w-[200px] min-w-[300px] max-w-[650px] md:hidden text-lg">
+                Tiene como objetivo principal dar a conocer la biodiversidad y geología de Gran Canaria a la 
+                ciudadanía de la isla, como vía fundamental para promaver la conservación del territorio, su biodiversidad y geodiversidad.
+            </div>
 
         </div>
 
