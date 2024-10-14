20af041829fb3639748008f10357c519485f4a5d
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-14 14:18:57 +0100

    Add AccodionItem test

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -1,9 +1,15 @@
 'use client';
 import { useState } from "react";
 
-const AccordionItem = ({title, description}:{title: string, description: string}) => {
+interface AccordionitemProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+    title: string,
+    description: string
+}
+
+export const AccordionItem = ({title, description, ...props}:AccordionitemProps) => {
     const [open, setOpen] = useState(false);
-    return <div className="mx-3 max-w-[90vw]">
+
+    return <div className="mx-3 max-w-[90vw]" {...props}>
       <h2 id="accordion-collapse-heading-1">
         <button type="button" 
             onClick={() => setOpen(!open)} 
@@ -18,7 +24,7 @@ const AccordionItem = ({title, description}:{title: string, description: string}
         >
             <span>{title}</span>
             <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
             </svg>
         </button>
     </h2>
