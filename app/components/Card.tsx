03f29d80bd319e944b9619ab95e8818e86fb58dd
Zixiong Lin Yang <zlinyan@gesplan.es>
2024-10-14 12:55:36 +0100

    Added Accordion for mobile devices

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,13 +1,13 @@
 export const Card = ({ title, description }: { title: string, description: string }) => {
 	return <div className="p-2 sm:p-8 bg-white md:bg-custom-light-bg text-custom-light-bg md:text-white rounded-md sm:rounded-lg
 						sm:min-w-[200px] min-w-[300px]
-                          max-w-[650px] border-custom-light-bg border-2 md:border-none">
+                         border-custom-light-bg border-2 md:border-none hidden md:block">
 		<div className="md:border-b-2 md:pb-4 mb-0 md:mb-4">
 			<h3 className="text-lg sm:text-2xl">
 				{title}
 			</h3>
 		</div>
-		<p className="text-xl hidden md:block">
+		<p className="text-xl ">
 			{description}
 		</p>
 	</div>
