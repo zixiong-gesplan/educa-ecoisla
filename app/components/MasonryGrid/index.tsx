0e57d888e2725b7b1033c7b99c295b8121f51447
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-17 11:02:24 +0100

    Modify transition

--- app/components/MasonryGrid/index.tsx
+++ app/components/MasonryGrid/index.tsx
@@ -29,7 +29,6 @@ export const MasonryGrid = ({ images }: { images: ImageData[] }) => {
         setSelectedImage("");
     };
 
-
     useEffect(() => {
         setIsVisible(isOpen);
     }, [isOpen]);
@@ -53,15 +52,17 @@ export const MasonryGrid = ({ images }: { images: ImageData[] }) => {
             </Masonry>
 
             {isOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
-                    <div className={`relative transition-opacity duration-1000 delay-200 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-                        <Image src={selectedImage} alt="Selected" className="w-full h-full max-w-full max-h-screen" />
-                        <button
-                            className="absolute top-2 right-4 text-white text-3xl"
-                            onClick={closeModal}
-                        >
-                            &times;
-                        </button>
+                <div className={`transition-opacity duration-1000 delay-200 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+                    <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+                        <div className="relative">
+                            <Image src={selectedImage} alt="Selected" className="w-full h-full max-w-full max-h-screen" />
+                            <button
+                                className="absolute top-2 right-4 text-white text-3xl"
+                                onClick={closeModal}
+                            >
+                                &times;
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
