2d1217791bab037f7534d734afbf2e53e13097fa
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-17 13:55:07 +0100

    Modify custom buttons margins and change the SVG for the gallery icon

--- app/components/Accordion.tsx
+++ app/components/Accordion.tsx
@@ -14,7 +14,7 @@ export const AccordionItem = ({title, description, ...props}:AccordionitemProps)
         <button type="button" 
             onClick={() => setOpen(!open)} 
             className={
-                `flex items-center justify-between w-full p-5 font-medium rtl:text-right  border border-gray-200 
+                `flex items-center justify-between p-5 font-medium rtl:text-right  border border-gray-200 
              dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400
                 hover:bg-custom-light-bg hover:text-white w-full
              dark:hover:bg-gray-800 gap-3
