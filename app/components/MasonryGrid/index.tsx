4f71fadfb47e5d2503f69427588fe30ec13f2555
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-17 09:28:23 +0100

    Replace img label for Image component

--- app/components/MasonryGrid/index.tsx
+++ app/components/MasonryGrid/index.tsx
@@ -49,9 +49,9 @@ export const MasonryGrid = ({ images }: { images: ImageData[] }) => {
             {isOpen && (
                 <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
                     <div className="relative">
-                        <img src={selectedImage} alt="Selected" className="max-w-full max-h-screen" />
+                        <Image src={selectedImage} alt="Selected" className="w-full h-full max-w-full max-h-screen" />
                         <button
-                            className="absolute top-4 right-4 text-white text-3xl"
+                            className="absolute top-2 right-4 text-white text-3xl"
                             onClick={closeModal}
                         >
                             &times;
