88eb8533389a1060248d52fedc76ae13c1361784
Juan Sebastián Ramirez Artiles <jramart@gesplan.es>
2024-10-23 09:56:52 +0100

    Fix dark mode text

--- app/sections/03-hero.tsx
+++ app/sections/03-hero.tsx
@@ -8,8 +8,8 @@ export const Hero = () => {
 		<section id="inicio">
 			<div className="grid max-w-screen-xl px-0 sm:px-2 py-8 mx-4 lg:gap-8 xl:gap-0 lg:py-16 xl:grid-cols-12">
 				<div className="mr-auto place-self-center xl:col-span-7">
-					<h2 className="mb-4 text-2xl sm:text-5xl">La educación ambiental supone una pieza importante en la conservación del territorio.</h2>
-					<p className="text-lg sm:text-2xl">La consejería de Medio Ambiente del Cabildo de Gran Canaria ha implementado un Plan de Acción de Educación Ambiental
+					<h2 className="mb-4 text-2xl sm:text-5xl dark:text-black">La educación ambiental supone una pieza importante en la conservación del territorio.</h2>
+					<p className="text-lg sm:text-2xl dark:text-black">La consejería de Medio Ambiente del Cabildo de Gran Canaria ha implementado un Plan de Acción de Educación Ambiental
 						para la Sostenibilidad, centrado en la concienciación social sobre la conservación del medio ambiente.
 					</p>
 					<div className="flex mt-10 sm:w-5/6 flex-row justify-start gap-2 sm:gap-3 md:gap-4" >
--- app/sections/07-actions.tsx
+++ app/sections/07-actions.tsx
@@ -3,37 +3,37 @@ import { ActivityTarget } from "../components/ActivityTarget";
 import { MoreActivities } from "../components/MoreActivities";
 
 export const Actions = () => {
-    return (
-        <section id="actividades" className="mx-4 max-w-screen-xl">
+	return (
+		<section id="actividades" className="mx-4 max-w-screen-xl">
 
-            <h2 className="m-6 text-2xl w-full text-center text-gray-900 font-extrabold">ACTIVIDADES</h2>
+			<h2 className="m-6 text-2xl w-full text-center text-gray-900 font-extrabold">ACTIVIDADES</h2>
 
-            <div className="grid grid-cols-3 lg:grid-cols-4 gap-3 xl:gap-12">
+			<div className="grid grid-cols-3 lg:grid-cols-4 gap-3 xl:gap-12 dark:text-black">
 
-                <ActivityTarget size={250} text="Talleres en aula" />
-                <ActivityTarget size={250} text="Salidas al campo" />
-                <ActivityTarget size={250} text="Rutas al campo" />
-                <ActivityTarget size={250} text="Ferias" />
-                <ActivityTarget size={250} text="Colaboraciones" />
+				<ActivityTarget size={250} text="Talleres en aula" />
+				<ActivityTarget size={250} text="Salidas al campo" />
+				<ActivityTarget size={250} text="Rutas al campo" />
+				<ActivityTarget size={250} text="Ferias" />
+				<ActivityTarget size={250} text="Colaboraciones" />
 
-                <MoreActivities text="Más actividades" />
+				<MoreActivities text="Más actividades" />
 
-            </div>
-            <div className=" w-full flex justify-center items-center">
+			</div>
+			<div className=" w-full flex justify-center items-center">
 
-                <Link
-                    href="/gallery"
-                    className="text-custom-light-bg text-lg sm:text-2xl text-center mt-6">
-                    Accede a todas las actividades realizadas
-                </Link>
+				<Link
+					href="/gallery"
+					className="text-custom-light-bg text-lg sm:text-2xl text-center mt-6">
+					Accede a todas las actividades realizadas
+				</Link>
 
-                <svg className="w-6 h-6 text-custom-light-bg dark:text-white  mt-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
-                </svg>
+				<svg className="w-6 h-6 text-custom-light-bg dark:text-white  mt-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+					<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 12H5m14 0-4 4m4-4-4-4" />
+				</svg>
 
-            </div>
+			</div>
 
-        </section>
-    );
+		</section>
+	);
 
 }
